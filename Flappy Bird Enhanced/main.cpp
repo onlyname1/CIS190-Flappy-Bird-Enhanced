038a940357f8683b4eaec14efc27ddc46b2bddb6
An Duong <onlyname@seas.upenn.edu>
2021-12-02 15:07:14 -0500

    added in background, assets, some logic

--- Flappy Bird Enhanced/main.cpp
+++ Flappy Bird Enhanced/main.cpp
@@ -1,10 +1,24 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(600, 600), "Flappy Bird Enhanced");
+    sf::RenderWindow window(sf::VideoMode(1024, 512), "Flappy Bird Enhanced");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
+    sf::Texture texture;
+    if (!texture.loadFromFile("..\\gameAssets\\Background\\Background5.png"))
+    {
+        std::cout << "error loading texture" << std::endl;
+        // error...
+    }
+    texture.setRepeated(true);
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+    sprite.setTextureRect(sf::IntRect(0, 0, 1024, 512));
+    
+    sprite.setScale(sf::Vector2f(2.f, 2.f));
+
 
     while (window.isOpen())
     {
@@ -19,7 +33,17 @@ int main()
         // erase previously drawn stuff
         window.clear();
 
+        shape.move(0, 0.1f);
+        if (shape.getPosition().y >= window.getSize().y)
+        {
+            shape.setPosition(sf::Vector2f(shape.getPosition().x, 0));
+        }
+        else if (shape.getPosition().y < 0)
+        {
+            shape.setPosition(sf::Vector2f(shape.getPosition().x, window.getSize().y));
+        }
         // draw stuff here
+        window.draw(sprite);
         window.draw(shape);
 
         // display frame
@@ -27,4 +51,9 @@ int main()
     }
 
     return 0;
+}
+
+void updateGame()
+{
+
 }
