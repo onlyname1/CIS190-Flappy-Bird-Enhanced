595188655dedf5e9c5ee842516b8b4885aff75b7
lancetan321 <lancetan450@gmail.com>
2021-12-04 13:48:24 -0500

    hi

--- Flappy Bird Enhanced/pipe.h
+++ Flappy Bird Enhanced/pipe.h
@@ -1,6 +1,19 @@
 #pragma once
+#include <SFML/Graphics.hpp>
 class pipe
 {
+private:
+	std::unique_ptr<sf::Sprite> spr;
 
+public:
+	// constructor
+	pipe(sf::Sprite sprite) 
+		: spr(std::make_unique<sf::Sprite>(sprite)) {}
+
+	// moving 
+	void move(sf::Vector2f);
+
+	// getBounds
+	sf::FloatRect getBounds();
 };
 
